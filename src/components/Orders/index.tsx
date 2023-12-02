5b2664e4e396c15df4e7e5cd7810ed32123a4382
umdevadriano <adrianosilvasampaio86@gmail.com>
2023-12-02 14:47:28 -0300

    lista de ordens com mais pedidos

--- src/components/Orders/index.tsx
+++ src/components/Orders/index.tsx
@@ -28,6 +28,131 @@ const orders:Order[] = [
         '_id': '655acb34de7c1a39486eb33b'
       }
     ],
+  }, {
+    '_id': '655acb34de7c1a39986eb339',
+    'table': '1',
+    'status': 'DONE',
+    'products': [
+      {
+        'product': {
+          'name': 'Pizza quatro queijos',
+          'imgePath': '1700444023001-coca-cola.png',
+          'price': 40,
+        },
+        'quantity': 60,
+        '_id': '655acb34de7c1a39486eb33a'
+      },
+      {
+        'product':
+        {
+          'name': 'Pizza quatro queijos',
+          'imgePath': '1700444023001-coca-cola.png',
+          'price': 40,
+        },
+        'quantity': 80,
+        '_id': '655acb34de7c1a39486eb33b'
+      }
+    ],
+  }, {
+    '_id': '655acb34de7c1039486eb339',
+    'table': '2',
+    'status': 'DONE',
+    'products': [
+      {
+        'product': {
+          'name': 'Pizza quatro queijos',
+          'imgePath': '1700444023001-coca-cola.png',
+          'price': 20,
+        },
+        'quantity': 6,
+        '_id': '655acb34de7c1a39486eb33a'
+      },
+      {
+        'product':
+        {
+          'name': 'Pizza quatro queijos',
+          'imgePath': '1700444023001-coca-cola.png',
+          'price': 40,
+        },
+        'quantity': 80,
+        '_id': '655acb34de7c1a39486eb33b'
+      }
+    ],
+  }, {
+    '_id': '655pcb34de7c1ap9486eb339',
+    'table': '3',
+    'status': 'DONE',
+    'products': [
+      {
+        'product': {
+          'name': 'Pizza quatro queijos',
+          'imgePath': '1700444023001-coca-cola.png',
+          'price': 40,
+        },
+        'quantity': 60,
+        '_id': '655acb34de7c1a39486eb33a'
+      },
+      {
+        'product':
+        {
+          'name': 'Pizza quatro queijos',
+          'imgePath': '1700444023001-coca-cola.png',
+          'price': 40,
+        },
+        'quantity': 80,
+        '_id': '655acb34de7c1a39486eb33b'
+      }
+    ],
+  }, {
+    '_id': '695acb34oe7c1a39486eb339',
+    'table': '4',
+    'status': 'IN_PRODUCTION',
+    'products': [
+      {
+        'product': {
+          'name': 'Pizza quatro queijos',
+          'imgePath': '1700444023001-coca-cola.png',
+          'price': 9,
+        },
+        'quantity': 1,
+        '_id': '655acb34de7c1a39486eb33a'
+      },
+      {
+        'product':
+        {
+          'name': 'Pizza quatro queijos',
+          'imgePath': '1700444023001-coca-cola.png',
+          'price': 5,
+        },
+        'quantity': 10,
+        '_id': '655acb34de7c1a39486eb33b'
+      }
+    ],
+  }, {
+    '_id': '655acb30de7c1pa39486eb339',
+    'table': '5',
+    'status': 'WATING',
+    'products': [
+      {
+        'product': {
+          'name': 'Pizza quatro queijos',
+          'imgePath': '1700444023001-coca-cola.png',
+          'price': 10,
+        },
+        'quantity': 1,
+        '_id': '655acb34de7c1a39486eb33a'
+      },
+      {
+        'product':
+        {
+          'name': 'Pizza quatro queijos',
+          'imgePath': '1700444023001-coca-cola.png',
+          'price': 4,
+        },
+        'quantity': 3,
+        '_id': '655acb34de7c1a39486eb33b'
+      }
+    ],
   }
 ];
 
